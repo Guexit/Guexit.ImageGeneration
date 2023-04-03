20ed027e96b2154c34ebe28288e7cf77a870be1a
ugm2 <unaigaraymaestre@gmail.com>
2023-04-03 13:07:46 +0200

    Adds all unittests to init
    Signed-off-by: ugm2 <unaigaraymaestre@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,11 +6,15 @@ import os
 import sys
 import unittest
 
+from tests.image_generation.test_image_generation_stable_diffusion_handler import (
+    TestStableDiffusionHandler,
+)
 from tests.services.test_image_generation_message_handler import (
     TestImageGenerationMessageHandler,
 )
 
 assert TestImageGenerationMessageHandler
+assert TestStableDiffusionHandler
 
 sys.path.append(os.getcwd())
 
